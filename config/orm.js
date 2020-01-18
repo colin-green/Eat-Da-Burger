5d64809ecfd152e0294fce711843f4ed6ccd61aa
Colin Green <colin.green@knights.ucf.edu>
2020-01-18 09:38:46 -0500

    Eat da burger in progress

--- config/orm.js
+++ config/orm.js
@@ -2,4 +2,28 @@ var connection = require("./connection.js");
 
 var orm = {
     
+    selectAll: function() {
+        var queryString = 'SELECT * FROM burgers';
+        connection.query(queryString, function(err, results) {
+            if (err) throw err;
+            console.log(result);
+        })
+    },
+    insertOne: function(burgerName) {
+        var queryString = 'INSERT INTO burgers (burger_name) VALUES (?)';
+        connection.query(queryString, [burgerName], function(err, results) {
+            if (err) throw err;
+            console.log(result);
+        })
+    },
+    updateOne: function(id, devoured) {
+        var queryString = 'UPDATE burgers SET devoured = ? WHERE id = ?';
+        connection.query(queryString, [devoured, id], function(err, results) {
+            if (err) throw err;
+            console.log(result);
+        })
+    }
+
 }
+
+module.exports = orm;
