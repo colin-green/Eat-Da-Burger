c21660f966e010a23b2ac19df2d63eb4b6efd1fb
Colin Green <colin.green@knights.ucf.edu>
2020-02-04 14:35:28 -0500

    Hooking up Jaws DB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
     host: "192.168.99.100",
     port: 3306,
     user: "root",
     password: "docker",
     database: "burgers_db"
   });
+}
   
   // Making the connection
   connection.connect(function(err) {
